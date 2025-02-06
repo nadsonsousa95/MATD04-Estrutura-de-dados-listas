4a5b802243081d061642338cca00aa3a6467ec99
nadsonsousa95 <nadsonbcs95@gmail.com>
2025-02-06 11:41:56 -0300

    Remoção de nó em árvore binária

--- Arvore/class_arvore.py
+++ Arvore/class_arvore.py
@@ -10,6 +10,7 @@ class Arvorebinaria:
     def __init__(self):
         self.raiz = None
 
+    #Se o valor de x for encontrado, retorna o nó que aponta para x
     def buscaIterativa(self, x):
         r = self.raiz
         while (r != None) and (r.dado != x):
@@ -19,11 +20,11 @@ class Arvorebinaria:
                 r = r.dir
         if r == None: 
             return False
-        else: return True      
+        else: return r     
         
 
     def inserir(self, valor):
-        if (self.buscaIterativa(valor) == True):
+        if (self.buscaIterativa(valor) != False):
             return False
         y = None
         no = No(valor)
@@ -40,19 +41,50 @@ class Arvorebinaria:
             y.esq = no
         else:
             y.dir = no
+    
+    # Substitui a subarvore com raiz no nó u pela subarvore com raiz no no v
+    def transplante(self, u, v):
+        if u.pai == None:
+            self.raiz = v
+        elif u == u.pai.esq:
+            u.pai.esq = v
+        else:
+            u.pai.dir = v
+        if v != None:
+            v.pai = u.pai
 
+
+    def remover(self, valor):
+        if (self.buscaIterativa(valor) != False):
+            no = self.buscaIterativa(valor)
+            if no.esq == None:
+                self.transplante(no, no.dir)
+            elif no.dir == None:
+                self.transplante(no, no.esq)
+            else:
+                y = self.MenorValor(no.dir)
+                if y.pai != no:
+                    self.transplante(y, y.dir)
+                    y.dir = no.dir
+                    y.pai.dir = y
+                self.transplante(no, y)
+                y.esq = no.esq
+                y.pai.esq = y
+
+
+    #Retorna no com maior valor de chave a partir de um nó
     def MenorValor(self, no):
         if no != None:
             while (no.esq != None):
                 no = no.esq
-            return no.dado
+            return no
     
-    #Retorna maior valor de chave
+    #Retorna no com maior valor de chave a partir de um nó
     def MaiorValor(self, no):
         if no != None:
             while (no.dir != None):
                 no = no.dir
-            return no.dado
+            return no
         
     def percursoemOrdem(self, no):
         if no != None:
@@ -72,6 +104,7 @@ class Arvorebinaria:
             self.percursoPreOrdem(no.esq)
             self.percursoPreOrdem(no.dir)
 
+    # O sucessor de um nó x é um nó com menor chave maior que a chave de x
     def sucessor(self, no):
         if no.dir != None:
             return self.MenorValor(no.dir)       
@@ -82,6 +115,7 @@ class Arvorebinaria:
         return y
 
 
+
 a = Arvorebinaria()
 a.inserir(80)
 a.inserir(50)
@@ -89,10 +123,12 @@ a.inserir(30)
 a.inserir(100)
 a.inserir(10)
 a.inserir(20)
-print(a.buscaIterativa(100)) #Retorna (True, 100)
+a.inserir(35)
+a.remover(35)
+print(a.buscaIterativa(100).dado) #Retorna (100)
 print(a.buscaIterativa(40)) #Retorna False
-print(a.MenorValor(a.raiz)) #Retorna 10
-print(a.MaiorValor(a.raiz)) #Retorna 100
+print(a.MenorValor(a.raiz).dado) #Retorna 10
+print(a.MaiorValor(a.raiz).dado) #Retorna 100
 print(' ')
 a.percursoemOrdem(a.raiz)
 print(' ')
@@ -100,4 +136,4 @@ a.percursoPreOrdem(a.raiz)
 print(' ')
 a.percursoPosOrdem(a.raiz)
 print(' ')
-print(a.sucessor(a.raiz))
+print(a.sucessor(a.raiz).dado)
